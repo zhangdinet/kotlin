fe90e0f1dbe2680cedb43de31311356be6c1849d
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2017-08-16 01:50:28 +0300

    Parcelable: Add INNERCLASS metadata to Creator factory class and its outer class

--- plugins/android-extensions/android-extensions-compiler/src/org/jetbrains/kotlin/android/parcel/ParcelableCodegenExtension.kt
+++ plugins/android-extensions/android-extensions-compiler/src/org/jetbrains/kotlin/android/parcel/ParcelableCodegenExtension.kt
@@ -247,9 +247,13 @@ open class ParcelableCodegenExtension : ExpressionCodegenExtension {
         val codegenForCreator = ImplementationBodyCodegen(
                 codegen.myClass, classContextForCreator, classBuilderForCreator, codegen.state, codegen.parentCodegen, false)
 
+
         classBuilderForCreator.defineClass(null, V1_6, ACC_PUBLIC or ACC_STATIC,
                               creatorAsmType.internalName, null, "java/lang/Object",
                               arrayOf("android/os/Parcelable\$Creator"))
+
+        codegen.v.visitInnerClass(creatorAsmType.internalName, containerAsmType.internalName, "Creator", ACC_PUBLIC or ACC_STATIC)
+        codegenForCreator.v.visitInnerClass(creatorAsmType.internalName, containerAsmType.internalName, "Creator", ACC_PUBLIC or ACC_STATIC)
 
         writeSyntheticClassMetadata(classBuilderForCreator, codegen.state)
 
