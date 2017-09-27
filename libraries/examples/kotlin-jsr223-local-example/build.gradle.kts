91316c357e61b30a25c76b5da529ce80fe532742
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-09-27 20:24:40 +0200

    Add jsr223 java9 tests with workaround for KT-18833

--- libraries/examples/kotlin-jsr223-local-example/build.gradle.kts
+++ libraries/examples/kotlin-jsr223-local-example/build.gradle.kts
@@ -3,6 +3,8 @@ description = "Sample Kotlin JSR 223 scripting jar with local (in-process) compi
 
 apply { plugin("kotlin") }
 
+val scriptCP by configurations.creating
+
 dependencies {
     compile(projectDist(":kotlin-stdlib"))
     compile(projectDist(":kotlin-script-runtime"))
@@ -11,6 +13,18 @@ dependencies {
     testCompile(projectDist(":kotlin-test:kotlin-test-junit"))
     testCompile(commonDep("junit:junit"))
     testRuntime(projectDist(":kotlin-reflect"))
+    scriptCP(projectDist(":kotlin-stdlib"))
+    scriptCP(projectDist(":kotlin-script-runtime"))
+    scriptCP(projectRuntimeJar(":kotlin-script-util"))
 }
 
 projectTest()
+
+projectTest("test9") {
+    val jdkPath = project.property("JDK_9")
+    onlyIf { jdkPath != null }
+    executable = "$jdkPath/bin/java"
+    doFirst {
+        systemProperty("kotlin.script.classpath", scriptCP.asFileTree.asPath)
+    }
+}
