54e7f2f32d65b65afa957f06a1618359e071ce03
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-11-02 12:39:49 +0100

    Convert all compiler modules to intellij plugin

--- build-common/build.gradle.kts
+++ build-common/build.gradle.kts
@@ -3,6 +3,8 @@ description = "Kotlin Build Common"
 
 apply { plugin("kotlin") }
 
+configureIntellijPlugin()
+
 dependencies {
     compileOnly(project(":core:util.runtime"))
     compileOnly(project(":compiler:util"))
@@ -11,15 +13,20 @@ dependencies {
     compileOnly(project(":js:js.serializer"))
     compileOnly(project(":js:js.frontend"))
     compileOnly(project(":kotlin-reflect-api"))
-    compileOnly(ideaSdkDeps("util"))
     testCompileOnly(project(":compiler:cli-common"))
     testCompile(projectTests(":compiler:tests-common"))
     testCompile(commonDep("junit:junit"))
     testCompile(protobufFull())
     testCompile(projectDist(":kotlin-stdlib"))
-    testCompileOnly(ideaSdkDeps("openapi"))
     testRuntime(projectDist(":kotlin-compiler"))
     testRuntime(projectDist(":kotlin-reflect"))
+}
+
+afterEvaluate {
+    dependencies {
+        compileOnly(intellij { include("util.jar") })
+        testCompileOnly(intellij { include("openapi.jar") })
+    }
 }
 
 sourceSets {
