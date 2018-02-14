23dd614a5c7ed729f294a6c0e0dd34e67513dd27
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-02-14 16:58:19 +0100

    Implement basic support of the new scripting in the frontend and plugin

--- compiler/frontend.script/build.gradle.kts
+++ compiler/frontend.script/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
 
 apply { plugin("kotlin") }
 apply { plugin("jps-compatible") }
@@ -7,6 +8,8 @@ jvmTarget = "1.6"
 dependencies {
     compile(project(":compiler:util"))
     compile(project(":compiler:frontend"))
+    compile(project(":kotlin-scripting-common"))
+    compile(project(":kotlin-scripting-jvm"))
     compile(projectDist(":kotlin-stdlib"))
     compileOnly(project(":kotlin-reflect-api"))
     compile(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-core")) { isTransitive = false }
@@ -19,3 +22,6 @@ sourceSets {
     "test" {}
 }
 
+kotlin {
+    experimental.coroutines = Coroutines.ENABLE
+}
