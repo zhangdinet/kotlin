c7f897a190f6aa1bc885dfb0fac18af24841dda5
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-02-14 17:05:37 +0100

    Implement base jvm scripting infrastructure

---
+++ libraries/scripting/jvm-host/build.gradle.kts
@@ -0,0 +1,27 @@
+
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+
+apply {
+    plugin("kotlin")
+}
+
+dependencies {
+    compile(project(":kotlin-script-runtime"))
+    compile(project(":kotlin-stdlib"))
+    compile(project(":kotlin-scripting-common"))
+    compile(project(":kotlin-scripting-jvm"))
+    compileOnly(project(":compiler:cli"))
+    compileOnly(intellijCoreDep())
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
+
+kotlin.experimental.coroutines = Coroutines.ENABLE
+
+standardPublicJars()
+
+publish()
+
