321fde569d145a34ed67107fb2dfc9661505ef86
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-02-14 17:05:37 +0100

    Implement base jvm scripting infrastructure

---
+++ libraries/scripting/jvm/src/kotlin/script/experimental/jvm/jvmScriptEvaluation.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("unused")
+
+package kotlin.script.experimental.jvm
+
+import kotlin.script.experimental.api.CompiledScript
+import kotlin.script.experimental.api.ScriptConfigurator
+import kotlin.script.experimental.api.ScriptRunner
+import kotlin.script.experimental.api.typedKey
+import kotlin.script.experimental.host.BasicScriptingHost
+
+open class JvmBasicScriptingHost<ScriptBase : Any, in CS : CompiledScript<ScriptBase>>(
+    configurationExtractor: ScriptConfigurator,
+    compiler: JvmScriptCompiler,
+    runner: ScriptRunner<ScriptBase>
+) : BasicScriptingHost<ScriptBase, CS>(configurationExtractor, compiler, runner)
+
+object JvmScriptEvaluationEnvironmentParams {
+    val baseClassLoader by typedKey<ClassLoader?>()
+}
