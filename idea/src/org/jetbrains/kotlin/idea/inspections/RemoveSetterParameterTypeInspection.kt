2f5cc4b6990f03737f3c71bf79ee8037153e0990
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-03-28 17:22:29 +0200

    (???) Optimization: RemoveSetterParameterTypeInspection

--- idea/src/org/jetbrains/kotlin/idea/inspections/RemoveSetterParameterTypeInspection.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/RemoveSetterParameterTypeInspection.kt
@@ -11,16 +11,18 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.kotlin.idea.intentions.RemoveExplicitTypeIntention
 import org.jetbrains.kotlin.idea.intentions.isSetterParameter
-import org.jetbrains.kotlin.psi.parameterVisitor
+import org.jetbrains.kotlin.psi.propertyAccessorVisitor
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 class RemoveSetterParameterTypeInspection : AbstractKotlinInspection() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return parameterVisitor { parameter ->
+        return propertyAccessorVisitor {
+            if (it.isGetter) return@propertyAccessorVisitor
+            val parameter = it.parameter ?: return@propertyAccessorVisitor
             val typeReference = parameter.takeIf { it.isSetterParameter }
                 ?.typeReference
-                ?.takeIf { it.endOffset > it.startOffset } ?: return@parameterVisitor
+                ?.takeIf { it.endOffset > it.startOffset } ?: return@propertyAccessorVisitor
             holder.registerProblem(
                 typeReference,
                 "Redundant setter parameter type",
